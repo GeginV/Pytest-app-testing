98741bd244124c1b330b5ea70ecbb4c61b7834bf
GeginV <68192910+GeginV@users.noreply.github.com>
2021-06-10 10:01:25 +0300

    some basic tests

---
+++ tests/basic_testing.py
@@ -0,0 +1,31 @@
+import pytest
+from app.calculator import Calculator
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_multiply_fail(self):
+        assert self.calc.multiply(self, 2, 2) == 5
+
+    def test_division(self):
+        assert self.calc.division(self, 10, 2) == 5
+
+    def test_division_fail(self):
+        assert self.calc.division(self, 10, 2) == 3
+
+    def test_subtraction(self):
+        assert self.calc.subtraction(self, 10, 2) == 8
+
+    def test_subtraction_fail(self):
+        assert self.calc.subtraction(self, 10, 2) == 5
+
+    def test_adding(self):
+        assert self.calc.adding(self, 10, 2) == 12
+
+    def test_adding_fail(self):
+        assert self.calc.adding(self, 10, 2) == 22
